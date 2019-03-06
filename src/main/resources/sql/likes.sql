b1a5cbb9f69722cabeaf31e500dd41261bf0bd03
github_username <chorobaev@inbox.ru>
2019-03-06 14:24:06 +0600

    test jenkins with git

--- src/main/resources/sql/likes.sql
+++
@@ -1,8 +0,0 @@
-/*
--- Query: SELECT * FROM hciproject.likes
-LIMIT 0, 1000
-
--- Date: 2019-02-06 13:38
-*/
-INSERT INTO `likes` (`id`,`app_id`,`username`,`blogid`) VALUES (98,8,'tansuluu.myrzaeva@iaau.edu.kg',0);
-INSERT INTO `likes` (`id`,`app_id`,`username`,`blogid`) VALUES (116,0,'iman@gmail.com',113);
--- src/main/resources/sql/role.sql
+++
@@ -1,9 +0,0 @@
-/*
--- Query: SELECT * FROM hciproject.role
-LIMIT 0, 1000
-
--- Date: 2019-02-06 13:40
-*/
-INSERT INTO `role` (`role_id`,`role`) VALUES (1,'ADMIN');
-INSERT INTO `role` (`role_id`,`role`) VALUES (2,'GID');
-INSERT INTO `role` (`role_id`,`role`) VALUES (3,'TOURIST');
